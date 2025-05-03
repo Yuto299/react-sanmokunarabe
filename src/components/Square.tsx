33e4c0d91c5a6f3b1d018f916e1ea579ff7881d2
Yuto299 <ota.se.ke@outlook.jp>
2025-05-03 12:22:59 +0900

    マスを押したらX表示

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,17 @@
-const Square = ({ value }: { value: string | number }) => {
-  return <button className='square'>{value}</button>;
+import { useState } from 'react';
+
+const Square = () => {
+  const [value, setValue] = useState<string | null>(null);
+
+  const handleClick = () => {
+    setValue('☓');
+  };
+
+  return (
+    <button className='square' onClick={handleClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
